8ece6e3d639734d24c9a4f88c4894aaea19f2b8d
Apurbo Kumar Anup <apurboka@gmail.com>
2023-04-14 10:37:06 +0600

    Update

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-const { createStore, combineReducers } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
 
 // products constants
 const GET_PRODUCTS = "GetProducts";
 const ADD_PRODUCT = "AddProduct";
-
-// cart constants
-const GET_CART_ITEMS = "GetCartItems";
-const ADD_CART_ITEM = "AddCartItem";
 
 
 // Product States
 const initialProductState = {
     products: ["suger", "Salt"],
     numberofProducts: 2
-}
-
-
-// Cart States
-const initialCartSate = {
-    cart: ["suger"],
-    numberofProducts: 1
 }
 
 
@@ -38,23 +28,7 @@ const AddProduct = (product) => {
 }
 
 
-
-// Cart actions
-const GetCart = () => {
-    return {
-        type: GET_CART_ITEMS,
-    }
-}
-
-const AddCart = (product) => {
-    return {
-        type: ADD_CART_ITEM,
-        payload: product
-    }
-}
-
-
-// productResource
+// productReducer
 const productReducer = (state = initialProductState, action) => {
     switch (action.type) {
         case GET_PRODUCTS:
@@ -74,39 +48,14 @@ const productReducer = (state = initialProductState, action) => {
 }
 
 
-// cartReducer
-const cartReducer = (state = initialCartSate, action) => {
-    switch (action.type) {
-        case GET_CART_ITEMS:
-            return {
-                ...state,
-            }
 
-        case ADD_CART_ITEM:
-            return {
-                cart: [...state.cart, action.payload],
-                numberofProducts: state.numberofProducts + 1
-            }
 
-        default:
-            return state;
-    }
-}
-
-const rootReducer = combineReducers({
-    productR: productReducer,
-    cartR: cartReducer
-})
 
 // store
-const store = createStore(rootReducer);
+const store = createStore(productReducer, applyMiddleware(logger));
 store.subscribe(() => {
     console.log(store.getState());
 });
 
 store.dispatch(getProduct());
 store.dispatch(AddProduct("pen"));
-
-
-store.dispatch(GetCart());
-store.dispatch(AddCart("pen"));
